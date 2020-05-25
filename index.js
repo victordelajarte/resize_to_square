22aeac1237f748c6a66c29897996d6bb9a75451f
victordelajarte <victordelajarte@hotmail.fr>
2020-05-26 00:26:32 +0200

    resize and square perfectly

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const sharp = require("sharp");
 
-const sourcePath = "inputs/clover_club.png";
-const outputPath = "outputs/clover_club.png";
-
 main();
 
-function main() {
-  sharp(sourcePath)
-    .resize(400)
-    .extend({
-      top: 10,
-      bottom: 10,
-      right: 100,
-      left: 56,
+async function main() {
+  const SIZE = 400;
+
+  const FILE_NAME = "clover_club.png";
+  const SOURCE_PATH = `inputs/${FILE_NAME}`;
+  const OUTPUT_PATH = `outputs/${FILE_NAME}`;
+
+  // trim: deletes the pixels that are the same color as the first top left pixel (here, transparent)
+  const file = sharp(SOURCE_PATH).trim();
+
+  await file
+    .resize(SIZE, SIZE, {
       background: { r: 0, g: 0, b: 0, alpha: 0 },
+      fit: "contain",
     })
-    .toFile(outputPath)
-    .then(() => console.log("toto"));
+    .toFile(OUTPUT_PATH);
+
+  console.log("finished !");
 }
