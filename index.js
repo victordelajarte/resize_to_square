fcfaf3f8d9180c6dc22372f04b09cb1643557ef7
victordelajarte <victordelajarte@hotmail.fr>
2020-05-25 23:59:25 +0200

    resize random

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const sharp = require("sharp");
 
-const sourcePath = "/inputs/clover_club.png";
-const outputPath = "/outputs/clover_club.png";
+const sourcePath = "inputs/clover_club.png";
+const outputPath = "outputs/clover_club.png";
 
 main();
 
-function main() {}
+function main() {
+  sharp(sourcePath)
+    .resize(400)
+    .extend({
+      top: 10,
+      bottom: 10,
+      right: 100,
+      left: 56,
+      background: { r: 0, g: 0, b: 0, alpha: 0 },
+    })
+    .toFile(outputPath)
+    .then(() => console.log("toto"));
+}
